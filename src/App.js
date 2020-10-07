af4c5f9b0c4a1000103b631a09f4582c9431241a
Guillaume Queste <guillaume.queste@laposte.net>
2020-10-07 11:55:23 +0200

    triangles debut

--- src/App.js
+++ src/App.js
@@ -10,51 +10,54 @@ const App = () => (
               link={"http://chevaux.guillaumequeste.fr"} />
     <Header />
     <div className="app">
+     
       <div className="ligne">
         <Link to="/presentation" className="case presentation">
           <div className="domaine">
-            <p className="titre_domaine">Presentation</p>
+            <p className="titre_domaine titre_presentation">Presentation</p>
           </div>
         </Link>
         <Link to="/etudes" className="case etudes">
           <div className="domaine">
-            <p className="titre_domaine">Etudes</p>
+            <p className="titre_domaine titre_etudes">Etudes</p>
           </div>
         </Link>
       </div>
       <div className="ligne">
         <Link to="/experiences_professionnelles" className="case experiences_professionnelles">
           <div className="domaine">
-            <p className="titre_domaine">Expériences professionnelles</p>
+            <p className="titre_domaine titre_experiences_professionnelles">Expériences professionnelles</p>
           </div>
         </Link>
         <Link to="/experiences_additionnelles" className="case experiences_additionnelles">
           <div className="domaine">
-            <p className="titre_domaine">Expériences additionnelles</p>
+            <p className="titre_domaine titre_experiences_additionnelles">Expériences additionnelles</p>
           </div>
         </Link>
       </div>
+      
+
       <div className="ligne">
         <Link to="/centres_d_interets" className="case centres_d_interets">
           <div className="domaine">
-            <p className="titre_domaine">Centres d'intérêts</p>
+            <p className="titre_domaine titre_centre_d_interets">Centres d'intérêts</p>
           </div>
         </Link>
         <Link to="/competences" className="case competences">
           <div className="domaine">
-            <p className="titre_domaine">Competences</p>
+            <p className="titre_domaine titre_competences">Competences</p>
           </div>
         </Link>
       </div>
       <div className="ligne">
         <Link to="/book" className="case book">
           <div className="domaine">
-            <p className="titre_domaine">Book</p>
+            <p className="titre_domaine titre_book">Book</p>
           </div>
         </Link>
         <Link to="/contact" className="case contact">
           <div className="domaine">
-            <p className="titre_domaine">Contact</p>
+            <p className="titre_domaine titre_contact">Contact</p>
           </div>
         </Link>
       </div>
