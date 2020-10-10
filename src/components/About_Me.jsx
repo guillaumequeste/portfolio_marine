d21111380a0c6f2c5816f82d0a643603f4b2d121
Guillaume Queste <guillaume.queste@laposte.net>
2020-10-10 17:33:51 +0200

    presentation about me

--- src/components/About_Me.jsx
+++ src/components/About_Me.jsx
@@ -18,10 +18,34 @@ class About_Me extends Component {
           <div className="detailsPresentation">
             <div className="container">
               <div className="photoMarine">
-                <p>photo</p>
+                <img
+                  src={require("../img/paris.jpg")}
+                  alt="photoMarine"
+                  className="imgPhotoMarine"
+                ></img>
               </div>
               <div className="description">
-                <p>texte</p>
+                <div className="texteDescription">
+                  <p className="titreDescription">Salut,</p>
+                  <p>
+                    Moi c'est Marine, j'ai 33 ans. Je suis une jeune femme
+                    pleine de vie, dynamique et créative.
+                  </p>
+                  <p>
+                    Après 7 ans en tant que chef de projet packaging, j'ai
+                    décidé de me reconvertir dans deux domaines qui me
+                    passionnent : l'évènementiel et la communication. Le
+                    changement, les défis et l'aventure ne me font pas peur. Je
+                    souhaiterais rejoindre une nouvelle équipe au sein de
+                    laquelle je saurai être force de proposition et où je
+                    m'investirai à fond !
+                  </p>
+                  <p>
+                    Comme le dit si bien Paul Morand "Le plus beau voyage, c'est
+                    celui qu'on fait l'un vers l'autre."
+                  </p>
+                  <p><b>Alors voyageon ensemble !</b></p>
+                </div>
               </div>
             </div>
             <div className="liens">
@@ -35,21 +59,27 @@ class About_Me extends Component {
               </div>
               <div className="comptes">
                 <div className="detailsComptes">
+                  <div>
                   <img
                     src={require("../img/facebook.png")}
                     alt="facebook"
                     className="facebook"
                   ></img>
+                  </div>
+                  <div>
                   <img
                     src={require("../img/instagram.png")}
                     alt="facebook"
                     className="instagram"
                   ></img>
+                  </div>
+                  <div>
                   <img
                     src={require("../img/linkedin.png")}
                     alt="facebook"
                     className="linkedin"
                   ></img>
+                  </div>
                 </div>
               </div>
             </div>
