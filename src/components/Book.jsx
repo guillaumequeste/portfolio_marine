bf120f780188ce25e3ab3bfd64f403aaf139250c
Guillaume Queste <guillaume.queste@laposte.net>
2020-10-12 13:25:01 +0200

    book1

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from "react"
-import Header from "./Header"
-import Helmet from "./Helmet"
+import React, { Component } from "react";
+import Header from "./Header";
+import Helmet from "./Helmet";
 import EnTete from "./EnTete";
 
 class Book extends Component {
@@ -15,7 +15,83 @@ class Book extends Component {
         <div className="bodyPage">
           <EnTete />
           <div className="detailsBook">
-            <p>book</p>
+            <div class="container">
+              <div class="row">
+                <div class="col-md-4 col-sm-6 col-12 divCarte">
+                  <div className="carte">
+                    <div className="couvCarte">
+                      <div className="rond">
+                        <div className="rond2">un</div>
+                      </div>
+                    </div>
+                    <div className="up">up</div>
+                    <div className="down">down</div>
+                  </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6 col-12 divCarte">
+                  <div className="carte">
+                    <div className="couvCarte">
+                      <div className="rond">
+                        <div className="rond2">deux</div>
+                      </div>
+                    </div>
+                    <div className="up">up</div>
+                    <div className="down">down</div>
+                  </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6 col-12 divCarte">
+                  <div className="carte">
+                    <div className="couvCarte">
+                      <div className="rond">
+                        <div className="rond2">trois</div>
+                      </div>
+                    </div>
+                    <div className="up">up</div>
+                    <div className="down">down</div>
+                  </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6 col-12 divCarte">
+                  <div className="carte">
+                    <div className="couvCarte">
+                      <div className="rond">
+                        <div className="rond2">quatre</div>
+                      </div>
+                    </div>
+                    <div className="up">up</div>
+                    <div className="down">down</div>
+                  </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6 col-12 divCarte">
+                  <div className="carte">
+                    <div className="couvCarte">
+                      <div className="rond">
+                        <div className="rond2">cinq</div>
+                      </div>
+                    </div>
+                    <div className="up">up</div>
+                    <div className="down">down</div>
+                  </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6 col-12 divCarte">
+                  <div className="carte">
+                    <div className="couvCarte">
+                      <div className="rond">
+                        <div className="rond2">six</div>
+                      </div>
+                    </div>
+                    <div className="up">up</div>
+                    <div className="down">down</div>
+                  </div>
+                </div>
+
+                
+              </div>
+            </div>
           </div>
         </div>
       </div>
